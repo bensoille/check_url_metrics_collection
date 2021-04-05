5998c8528cb5e0be09ab9719708f86ae793b2671
Ben Soille <benjamin.soille@easiware.fr>
2021-04-05 15:20:12 +0200

    Made loop in requester / producer, moved pip requirements file for testing

--- check_url/measurement/src/measurement.py
+++ check_url/measurement/src/measurement.py
@@ -1,5 +1,6 @@
+import os
 import requests 
-import time
+import time, threading, signal
 from urllib.parse import urlparse
 
 class Measurement:
@@ -17,6 +18,7 @@ class Measurement:
     """
     self.url = url
     self.period = period
+    #self.originip = os.environ['']
 
 
   def get_url_response_time(self):
@@ -34,12 +36,15 @@ class Measurement:
     try:
       # Beginning tick, then request
       begin = time.time_ns()
-      r = requests.get(self.url)
+      r = requests.get(self.url, timeout=30, stream=True)
       # Response received tick
       inter = time.time_ns()
 
-      statuscode = r.status_code
-      receivedtext = r.text
+      # Store http request details
+      originip        = r.raw._original_response.fp.raw._sock.getsockname()[0]
+      targetip        = r.raw._original_response.fp.raw._sock.getpeername()[0]
+      statuscode      = r.status_code
+      receivedtext    = r.text
 
     except :
       print("Should send message to DLQ")
@@ -51,10 +56,50 @@ class Measurement:
     # Ticks were in ns : convert to ms
     metricsDict['resptimems']   = (inter - begin) / 1000000 
     metricsDict['receivedtext'] = receivedtext
-
+    metricsDict['targetip']     = targetip
+    metricsDict['sourceip']     = originip
+    metricsDict['logdate']      = r.headers['Date']
     return metricsDict
 
 
+  def start_periodic_requests(self):
+    self.ticker = threading.Event()
+    while not self.ticker.wait(self.period):
+      metrics = instance.get_url_response_time()
+      print(metrics)
+
+
+  def stop_periodic_requests(self):
+    self.ticker.clear()
+
+
+# Custom error for sys kill interrutions
+class ProgramKilled(Exception):
+    pass
+
+def signal_handler(signum, frame):
+    raise ProgramKilled
+
+
 if(__name__) == '__main__':
-  instance = Measurement('http://soille.fr/hop.html', 5)
-  print(instance.get_url_response_time())
+  # Check that we got our env vars set and save resources on error
+  if not "CHECK_TARGET_URL" in os.environ or not "CHECK_PERIOD_SECONDS" in os.environ :
+    print("Some env vars are missing")
+    exit(1)
+    
+  # Catch system calls and allow clean shutdown
+  signal.signal(signal.SIGTERM, signal_handler)
+  signal.signal(signal.SIGINT, signal_handler)
+
+  try:
+    instance = Measurement(
+      os.environ['CHECK_TARGET_URL'], 
+      int(os.environ['CHECK_PERIOD_SECONDS'])
+    )
+    instance.start_periodic_requests()
+  except ProgramKilled:
+    print("Killing, please wait for clean shutdown")
+    instance.stop_periodic_requests()
+    # Wait a couple of seconds
+    time.sleep(2)
+    exit(0)
